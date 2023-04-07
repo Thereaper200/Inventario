24a25368d2620ea1d1063ed186643d4c6e313b2e
Thereaper200 <113812163+Thereaper200@users.noreply.github.com>
2023-04-06 23:43:28 -0700

    Add files via upload
    Inventario 2.0

--- Inventario.py
+++ Inventario.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import *
+from tkinter import messagebox
 from PIL import ImageTk, Image
 from pymongo import MongoClient
 from datetime import datetime
@@ -9,6 +10,12 @@ client = MongoClient('localhost', 27017)
 db = client.inventario
 
 coleccion = db.Inventarios_2023
+
+lista_oficinas = ['Oficina 2', 'Oficina 3', 'Oficina 4', 'Oficina 5', 'QC', 'Incubadora', 'AGTECH']
+
+lista_turnos = ['Turno Diurno', 'Turno Nocturno']
+
+lista_horario = ['Inicio', 'Final']
 
 
 def send_data():
@@ -20,7 +27,11 @@ def send_data():
     base1_data = base1.get()
     base2_data = base2.get()
 
-    oficina2_data = oficina2.get()
+    oficina_data = valor_oficnas.get()
+    turno_data = valor_turno.get()
+    horario_data = valor_horario.get()
+
+    '''oficina2_data = oficina2.get()
     oficina3_data = oficina3.get()
     oficina4_data = oficina4.get()
     oficina5_data = oficina5.get()
@@ -30,17 +41,20 @@ def send_data():
 
     dayshift_data = dayshift.get()
     nightshift_day = nightshift.get()
-    turno_data = [dayshift_data, nightshift_day]
+    turno_data = [dayshift_data, nightshift_day]'''
 
-    coleccion.insert_one({'Oficina': oficina_data, 'turno': turno_data, 'iPads': ipad_data, 'Lapices': lapiz_data,
-                          'Cables': cable_data, 'Adaptadores': adaptador_data,
+    coleccion.insert_one({'Oficina': oficina_data, 'turno': turno_data, 'horario': horario_data, 'iPads': ipad_data,
+                          'Lapices': lapiz_data, 'Cables': cable_data, 'Adaptadores': adaptador_data,
                           'Bases 1ra Generacion': base1_data, 'Base 2da Generacion': base2_data, 'fecha': fecha})
+
+    messagebox.showinfo("showinfo", "Inventario Agregado con Exito")
 
 
 mywindow = Tk()
 mywindow.geometry("650x650")
 mywindow.title("Inventario | Alpha Version")
 mywindow.resizable(False, False)
+mywindow.iconbitmap('ProjectIcon.ico')
 mywindow.config(background="#F0F8FF")
 main_title = Label(text="Inventario", font=("Cambria", 13), bg="#8B0000", fg="white", width="550", height="2")
 main_title.pack()
@@ -70,8 +84,11 @@ cable = IntVar()
 adaptador = IntVar()
 base1 = IntVar()
 base2 = IntVar()
+valor_oficnas = tkinter.StringVar(mywindow)
+valor_turno = tkinter.StringVar(mywindow)
+valor_horario = tkinter.StringVar(mywindow)
 
-oficina2 = IntVar()
+'''oficina2 = IntVar()
 oficina3 = IntVar()
 oficina4 = IntVar()
 oficina5 = IntVar()
@@ -79,8 +96,28 @@ qc = StringVar()
 incubadora = StringVar()
 
 dayshift = StringVar()
-nightshift = StringVar()
+nightshift = StringVar()'''
 
+
+valor_oficnas.set('Selecciona Una Oficina')
+
+menu_oficinas = tkinter.OptionMenu(mywindow, valor_oficnas, *lista_oficinas)
+menu_oficinas.config(bg='white', fg='black')
+menu_oficinas.place(x=22, y=38)
+
+valor_turno.set('Selecciona Un Turno')
+
+menu_turno = tkinter.OptionMenu(mywindow, valor_turno, *lista_turnos)
+menu_turno.config(bg='white', fg='black')
+menu_turno.place(x=210, y=38)
+
+valor_horario.set('Selecciona Una Opcion')
+
+menu_horario = tkinter.OptionMenu(mywindow, valor_horario, *lista_horario)
+menu_horario.config(bg='white', fg='black')
+menu_horario.place(x=385, y=38)
+
+'''
 dayshift_entry = Checkbutton(mywindow, text="Turno Diurno", variable=dayshift,
                              onvalue='Dia', offvalue=False, bg='#F0F8FF', fg='black')
 dayshift_entry.place(x=500, y=38)
@@ -106,7 +143,7 @@ qc_entry = Checkbutton(mywindow, text="QC", variable=qc,
 qc_entry.place(x=226, y=38)
 incubadora_entry = Checkbutton(mywindow, text="Incubadora", variable=incubadora,
                                onvalue='Incubadora', offvalue=False, bg='#F0F8FF', fg='black')
-incubadora_entry.place(x=277, y=38)
+incubadora_entry.place(x=277, y=38)'''
 
 ipad_entry = Entry(textvariable=ipad, width=40)
 ipad_entry.place(x=22, y=100)
